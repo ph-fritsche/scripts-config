01b17f17cc75492ebc13b35a602a22a997278155
Philipp Fritsche <ph.fritsche@gmail.com>
2022-04-28 18:35:08 +0000

    debugConsole

--- scripts.config.js
+++ scripts.config.js
@@ -1,1 +1,17 @@
-module.exports = require('./dist/config')
+const debugConsole = {
+    run: async () => {
+        process.stdout.write(`write plain\n`)
+
+        process.stdout.write(`console width: ${process.stdout.columns}\n`)
+        process.stdout.write(`write\x1B[33myellow\n`)
+        process.stdout.write(`write\x1B[32mgreen\n`)
+        process.stdout.write(`reset\x1B[0mcolor\n`)
+        process.stdout.write(`write\x1B[38;5;8mgray\n`)
+    },
+}
+
+module.exports = {
+    scripts: {
+        debugConsole,
+    },
+}
