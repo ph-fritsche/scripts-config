44fa4c7b0062ba975842ce1b20136730f8076466
Philipp Fritsche <ph.fritsche@gmail.com>
2022-04-29 20:08:24 +0000

    fix(ts-build2): override `noEmit`

--- src/ts-build2.ts
+++ src/ts-build2.ts
@@ -235,13 +235,16 @@ async function buildTypes(
     })
     fs.writeFileSync(tmpFile.name, JSON.stringify({
         extends: path.resolve(tsconfigFile),
+        compilerOptions: {
+            noEmit: false,
+            declaration: true,
+            emitDeclarationOnly: true,
+        },
         include: files.map(f => path.resolve(f)),
     }))
 
     await runChild(getTscBin(), [
         '--project', tmpFile.name,
         '--outDir', typesPath,
-        '--declaration', 'true',
-        '--emitDeclarationOnly',
     ]).finally(tmpFile.removeCallback)
 }
