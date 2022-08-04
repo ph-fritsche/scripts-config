d92b29db80c7996eafdfbaa7f037d2faa879656c
Philipp Fritsche <ph.fritsche@gmail.com>
2022-08-04 20:39:54 +0000

    test: fix mock

--- test/util/execScript.ts
+++ test/util/execScript.ts
@@ -1,7 +1,10 @@
-import { exit } from 'process'
+import process from 'process'
 import { execScript } from '../../src/util'
 
 test('exit when child process fails', () => {
+    const exit = jest.spyOn(process, 'exit')
+        .mockImplementationOnce(() => void 0 as never)
+
     execScript(['exit', '2'])
 
     expect(exit).toBeCalledWith(1)
